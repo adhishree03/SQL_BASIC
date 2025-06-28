78d945adfef94e5050675d5c66da64365db2840b
adhishree03 <146438711+adhishree03@users.noreply.github.com>
2025-06-29 03:17:28 +0530

    operators_whereMEMBERSHIP

---
+++ operators_whereMEMBERSHIP.sql
@@ -0,0 +1,25 @@
+/*OPERATORS
+
+=> MEMBERSHIP OPERATOR
+
+ 1) IN - check if a value exist in a list
+
+  QUES)  RETRIEVE ALL CUSTOMERS EITHER FROM GERMANY OR USA
+   
+   SELECT*
+   FROM customers
+   WHERE country =' Germany' OR country = 'USA'
+
+   
+  2) NOT IN
+
+ QUES)  RETRIEVE ALL CUSTOMERS WHO ARE NOT IN  GERMANY OR USA
+   SELECT*
+   FROM customers
+   WHERE country IN(' Germany' , 'USA')*/
+
+   
+
+   
+ 
+  
